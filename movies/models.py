2f38a2956eaded34b131389352c94d196be45f5d
nicokernelpanic <bohooobo@gmail.com>
2024-07-13 15:44:04 +0000

    Listo

--- movies/models.py
+++ movies/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Con estas clases gestionaremos la conexion a la base de datos y utilizaremos la clase Movie para la creacion de una tabla con el mismo nombre donde especificaremos los detalles de las peliculas
 
 class Movie(models.Model):
+    movie_id = models.IntegerField(null=False)
     name = models.CharField(max_length=30, null=False)
     genre = models.CharField(max_length=30, null=False)
     director_name = models.CharField(max_length=30, null=False)
