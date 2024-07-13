b57b1a9775600f3fca47b7efc239cd5599702601
nicokernelpanic <bohooobo@gmail.com>
2024-07-13 15:25:03 +0000

    Add files via upload

--- movies/models.py
+++ movies/models.py
@@ -3,5 +3,11 @@ from django.db import models
 # Con estas clases gestionaremos la conexion a la base de datos y utilizaremos la clase Movie para la creacion de una tabla con el mismo nombre donde especificaremos los detalles de las peliculas
 
 class Movie(models.Model):
-    movie_id = models.IntegerField(max_length=10, null=False)
-    name =  
+    name = models.CharField(max_length=30, null=False)
+    genre = models.CharField(max_length=30, null=False)
+    director_name = models.CharField(max_length=30, null=False)
+    year = models.IntegerField(null=False)
+    synopsis = models.CharField(max_length=150, null=False)
+
+    def __str__(self) -> str:
+        return self.name
--- movies/views.py
+++ movies/views.py
@@ -1,17 +1,18 @@
 from django.shortcuts import render
 from django.template import  loader
 from .models import Movie
+from django.http import HttpResponse
 
 def index(request):
-   movies = Movies.objects.order_by('type')
+   movies = Movie.objects.order_by('genre')
    template = loader.get_template('index.html')
    return HttpResponse(template.render({'movies':movies}, request))
 
 
 def movie(request, movie_id):
-    movie = Movie.objects.get(pk = pokemon_id)
+    movie = Movie.objects.get(pk = movie_id)
     template = loader.get_template('display_movie.html')
     context = {
             'movie': movie
             }
-    return HttpResponse(template.tender(context, request))
+    return HttpResponse(template.render(context, request))
